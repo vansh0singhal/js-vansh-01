8337a0585b1fb7c73c1cf44db7a260f6b133ad9d
vansh0singhal <109502651+vansh0singhal@users.noreply.github.com>
2024-10-28 23:07:14 +0530

    Add files via upload

---
+++ lecture2.js
@@ -0,0 +1,31 @@
+"use strict";//this say that we are using newer version of the java script
+
+//alert(2+2)
+//shows errot because we are using node js not browser this is only used in browser console
+
+console.log(2+3)
+
+console.log("vansh Singhal");
+//code rediability sshould be high
+
+let name="Vansh"
+let age=21;
+let isLoggedIn=true;
+
+//Types of data types
+//number
+//big int
+//boolean(true , false)
+//string
+//null is a stand alone value;
+//undefined
+let state=null;
+console.log(state)
+//symbol-> for uniqueness
+//object
+console.log(typeof(age));
+
+ //type of undefined is undefined
+ //type of null is object
+
+ 
